192c27a4d0b4ec9b6a7cfe83a6ee4c10c656fb74
KierkegaardRichIE <kierke7@outlook.com>
2020-03-13 00:15:19 +0800

    💦使用do-while语句对输入合法性进行检查， 可以有效完成防御性程序设计， 在遇到非法性输入时避免出错， 以增强程序健壮性。

--- main.cpp
+++ main.cpp
@@ -1,25 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
-long Fact(int n);
+unsigned long Fact(unsigned int n);
 int main()
 {
     int m;
-    long ret;
-    printf("Input m:");
-    scanf_s("%d", &m);
-    ret = Fact(m);
-    printf("%d!=%ld\n", m, ret);
+    do
+    {
+        printf("Input m:");
+        scanf_s("%d", &m);
+    } while (m < 0);
+    printf("%d!=%lu\n", m, Fact(m));
     printf("press any key to continue.");
     return 0;
 }
 
-long Fact(int n)
+unsigned long Fact(unsigned int n)
 {
-    int i;
-    long result = 1;
+    unsigned int i;
+    unsigned long result = 1;
     for (i = 2; i <= n; i++)
-    {
         result *= i;
-    }
     return result;
 }
